7affd7092feaffb1664de1c9106c95c2901ed31f
Samas1503 <https://github.com/Samas1503>
2021-05-07 16:35:39 -0300

    correccion de archivos .html

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,10 @@
+/*function confirmarEliminar(id) {
+    $('#modalEliminar').modal('show');
+    $("#identificadorUsuarioHiddenInput").val(id);
+}
+
+function eliminarUsuario() {
+    var id = $("#identificadorUsuarioHiddenInput").val();
+    window.location = "/cliente/eliminar/" + id;
+}*/
+console.log("hola");
