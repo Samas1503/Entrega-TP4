d2de9551e2fef3fe7fd0f0c418e50128641f3b98
Samas1503 <samueleliasparedes.10@gmail.com>
2021-05-14 14:49:30 -0300

    agregando clases para conectar con la base de datos

---
+++ src/main/java/ar/edu/unju/edm/tp4/service/imp/ClienteServiceMySQL.java
@@ -0,0 +1,63 @@
+package ar.edu.unju.edm.tp4.service.imp;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import ar.edu.unju.edm.tp4.model.Clientes;
+import ar.edu.unju.edm.tp4.service.IClienteDAO;
+import ar.edu.unju.edm.tp4.service.IClienteService;
+
+@Service
+@Qualifier("dataBaseImp")
+public class ClienteServiceMySQL implements IClienteService{
+
+    @Autowired
+	Clientes unCliente;
+	
+	@Autowired
+	IClienteDAO clienteDAO;
+
+    @Override
+    public List<Clientes> obtenerTodosClientes() {
+        return (List<Clientes>) clienteDAO.findAll();
+    }
+
+    @Override
+    public boolean verificarCliente(String tipo, int numDoc, String password) {
+        return false;
+    }
+
+    @Override
+    public void guardarCliente(Clientes unCliente) {
+        
+    }
+
+    @Override
+    public void adiconalesCliente(Clientes unCliente) {
+        
+    }
+
+    @Override
+    public Clientes crearCliente() {
+        return null;
+    }
+
+    @Override
+    public Clientes buscarCliente(int dni) {
+        return null;
+    }
+
+    @Override
+    public void modificarCliente(Clientes clienteModificado) {
+        
+    }
+
+    @Override
+    public void eliminarCliente(int dni) {
+        
+    }
+    
+}
